6e1ce4e22b7f08bc0b72471e08114b3d331f92de
Tyler Fabian Chargualaf <tchar003@wch136-23.cs.ucr.edu>
2020-04-15 15:49:59 -0700

    Completed part 2

---
+++ Lab2/turnin/tchar003_lab2_part2.c
@@ -0,0 +1,42 @@
+/*	Author: tchar003
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+unsigned char countSetBits(unsigned char n) 
+{ 
+    unsigned char count = 0; 
+    while (n) { 
+        count += n & 1; 
+        n >>= 1; 
+    } 
+    return count; 
+} 
+
+
+
+
+int main(void) {
+	DDRA = 0x00; PORTA = 0xFF; //Configure port A's 8 pins as inputs
+	DDRC = 0xFF; PORTC = 0x00; //Configure port B's 8 pins as ouputs, initialize to 0s
+	unsigned char cntavail = 0x00; // Temporary variable to hold the value of C
+	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+    	while (1) {
+		// 1) Read input (rightmost 4 bits)
+		tmpA = PINA & 0x0F;
+		cntavail = 4 - countSetBits(tmpA); //Subtract number of taken spaces from 4
+		
+		PORTC = cntavail;
+	}	
+    	return 0;
+}
