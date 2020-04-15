86f3f634d56b030546de47d63a6fe50685af5248
Tyler Fabian Chargualaf <tchar003@wch136-23.cs.ucr.edu>
2020-04-15 15:49:12 -0700

    Completed part 1

--- Lab2/turnin/tchar003_lab2_part1.c
+++ Lab2/turnin/tchar003_lab2_part1.c
@@ -7,21 +7,28 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
+
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
 int main(void) {
-	DDRA = 0x00; PORTA = 0xFF; // input
-	DDRB = 0xFF; PORTB = 0x00; //output
-	
-	unsigned char tempB = 0x00;	
-
+	DDRA = 0x00; PORTA = 0xFF; //Configure port A's 8 pins as inputs
+	DDRB = 0xFF; PORTB = 0x00; //Configure port B's 8 pins as ouputs, initialize to 0s
+	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
     	while (1) {
-		tempB = 0x00;
-		tempB = ~PINA;
-		PORTB = tempB;
-    	}	
-    	return 1;
+		// 1) Read input
+		tmpA = PINA & 0x03;
+		// if tmpA == 0x01, the door is open and there is not light
+    		if (tmpA == 0x01) { // True if PA1PA0 is 01
+			tmpB = 0x01; // Sets tmpB to bbbbbb01
+		} else {
+			tmpB = 0x00; // Sets tmpB to bbbbbb00
+		}
+		// 3) Write output
+		PORTB = tmpB;
+	}	
+    	return 0;
 }
